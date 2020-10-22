4030e7e1f2b66bde24487ae9b4adc1d4c96fcf0f
Jesse Bowling <jesse.bowling@duke.edu>
2020-10-22 14:38:11 -0400

    Create a new dictionary to ensure timestamp is the first JSON formatted key to be emitted
    Signed-off-by: Jesse Bowling <jesse.bowling@duke.edu>

--- hpfeeds-logger/hpfeedslogger/formatters/json_formatter.py
+++ hpfeeds-logger/hpfeedslogger/formatters/json_formatter.py
@@ -4,8 +4,14 @@ import time
 
 def format(message):
     msg = dict(message)
+    log = dict()
     t = datetime.datetime.isoformat(datetime.datetime.utcnow())
     if time.tzname[0] == 'UTC':
         t += 'Z'
     msg['timestamp'] = t
-    return json.dumps(msg)
+    # create a new object with timestamp first so it's output first
+    # This works on Python 3.6 and later as dictionaries keys are ordered by creation
+    log['timestamp'] = msg.pop('timestamp')
+    for item in msg:
+        log[item]=msg[item]
+    return json.dumps(log)
